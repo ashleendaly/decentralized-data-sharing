d77e00e4ebf0660f5ad0f75255c7d7f58f9e79bf
Ashleen Daly <ashddev@proton.me>
2024-01-16 00:10:13 +0000

    add data form

--- dapp/src/app/page.tsx
+++ dapp/src/app/page.tsx
@@ -2,7 +2,7 @@ import DataForm from "@/components/DataForm";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-w-[100vh] h-[100vh] grid place-items-center">
       <DataForm />
     </main>
   );
--- dapp/src/components/DataForm.tsx
+++ dapp/src/components/DataForm.tsx
@@ -2,23 +2,79 @@
 
 import { useStorageUpload } from "@/lib/thirdweb-dev";
 import { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Trash, Plus } from "lucide-react";
+
+interface ipfsData {
+  [key: string]: string;
+}
 
 export default function DataForm() {
-  const [text, setText] = useState("");
+  const [newProperty, setNewProperty] = useState<string>("");
+  const [dataToUploadToIpfs, setDataToUploadToIpfs] = useState<ipfsData>({});
   const { mutateAsync: upload, isLoading } = useStorageUpload();
+
+  const handleAddNewProperty = (property: string) => {
+    if (property != "") {
+      const propertyCapitalised = property[0].toUpperCase() + property.slice(1);
+      setDataToUploadToIpfs((prev) => ({
+        ...prev,
+        [propertyCapitalised]: "",
+      }));
+      setNewProperty("");
+    }
+  };
+
+  const handleUpdateProperty = (property: string, data: string) => {
+    setDataToUploadToIpfs((prev) => ({
+      ...prev,
+      [property]: data,
+    }));
+  };
+
+  const handleDeleteProperty = (property: string) => {
+    setDataToUploadToIpfs((prev) => {
+      const { [property]: deletedType, ...rest } = prev;
+      return rest;
+    });
+  };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    const uris = await upload({ data: [text] });
+    const uris = await upload({ data: [dataToUploadToIpfs] });
     console.log(uris);
   };
 
   return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <input value={text} onChange={(event) => setText(event.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
-    </main>
+    <form onSubmit={handleSubmit} className="flex flex-col w-1/3">
+      <div className="flex items-center">
+        <Input
+          placeholder="Add property..."
+          value={newProperty}
+          onChange={(event) => setNewProperty(event.target.value)}
+        />
+        <Button type="button" onClick={() => handleAddNewProperty(newProperty)}>
+          <Plus size={16} />
+        </Button>
+      </div>
+      {Object.keys(dataToUploadToIpfs).map((key) => {
+        return (
+          <div key={key} className="flex gap-1 items-center">
+            <p>{key}: </p>
+            <Input
+              value={dataToUploadToIpfs[key]}
+              onChange={(event) =>
+                handleUpdateProperty(key, event?.target.value)
+              }
+            />
+            <Button type="button" onClick={() => handleDeleteProperty(key)}>
+              <Trash size={16} />
+            </Button>
+          </div>
+        );
+      })}
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }
