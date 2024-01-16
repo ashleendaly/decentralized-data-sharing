1ae156bcf04f432fd7dc2708db70742f9d02b6db
Ashleen Daly <ashddev@proton.me>
2024-01-16 10:36:21 +0000

    reduced header

--- dapp/src/components/header.tsx
+++ dapp/src/components/header.tsx
@@ -1,34 +1,18 @@
 "use client";
 
-import {
-  useAddress,
-  useLogin,
-  useLogout,
-  useUser,
-  useConnect,
-  metamaskWallet,
-} from "@thirdweb-dev/react";
+import { useAddress, useConnect, metamaskWallet } from "@thirdweb-dev/react";
 
 export default function Header() {
   const metamaskConfig = metamaskWallet();
   const address = useAddress();
   const connect = useConnect();
-  const { login } = useLogin();
-  const { logout } = useLogout();
-  const { user, isLoggedIn } = useUser();
 
   return (
     <div>
-      {isLoggedIn ? (
-        <button onClick={() => logout()}>Logout</button>
-      ) : address ? (
-        <button onClick={() => login()}>Login</button>
-      ) : (
+      {!address && (
         <button onClick={() => connect(metamaskConfig)}>Connect</button>
       )}
-
       <pre>Connected Wallet: {address}</pre>
-      <pre>User: {user?.address || "N/A"}</pre>
     </div>
   );
 }
