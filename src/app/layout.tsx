6f11b51528dca2838e0523be776d0729030410b4
ashleendaly <Ashleen.Daly@res-group.com>
2024-02-27 11:46:19 +0000

    add secret key to thirdweb to get ipfs data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThirdwebProvider } from "@/lib/thirdweb-dev";
-import { Sepolia } from "@thirdweb-dev/chains";
-import Header from "@/components/header";
-import { Providers } from "./providers";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThirdwebProvider } from '@/lib/thirdweb-dev';
+import { Sepolia } from '@thirdweb-dev/chains';
+import Header from '@/components/header';
+import { Providers } from './providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Decentralised Personal Data Manager",
+  title: 'Decentralised Personal Data Manager',
   description:
-    "A personal data manager application that is powered by the InterPlanetary FileSystem and Ethereum blockchain.",
+    'A personal data manager application that is powered by the InterPlanetary FileSystem and Ethereum blockchain.',
 };
 
 export default function RootLayout({
@@ -23,10 +23,11 @@ export default function RootLayout({
     <Providers>
       <ThirdwebProvider
         clientId={process.env.THIRDWEB_CLIENT_ID}
+        secretKey={process.env.THIRDWEB_API_KEY}
         activeChain={Sepolia}
         authConfig={{
-          domain: process.env.APP_URL || "localhost:3000",
-          authUrl: "/api/auth",
+          domain: process.env.APP_URL || 'localhost:3000',
+          authUrl: '/api/auth',
         }}
       >
         <html lang="en">
