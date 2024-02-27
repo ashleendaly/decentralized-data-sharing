4283a9ab669932cb62e45a4c6c799518f407d0d6
ashleendaly <Ashleen.Daly@res-group.com>
2024-02-27 11:45:46 +0000

    changes that fix keygen and decrypt functions

--- rabe-wasm/src/lib.rs
+++ rabe-wasm/src/lib.rs
@@ -1,8 +1,9 @@
 extern crate rabe;
 use crate::rabe::schemes::*;
-use rabe::schemes::bsw::{CpAbeCiphertext,CpAbePublicKey };
+use rabe::schemes::bsw::{CpAbeCiphertext,CpAbePublicKey, CpAbeSecretKey, CpAbeMasterKey};
 use rabe::utils::policy::pest::PolicyLanguage;
 use wasm_bindgen::prelude::*;
+use js_sys::Uint8Array;
 use serde_json; 
 
 #[wasm_bindgen]
@@ -41,32 +42,32 @@ pub fn encrypt(pk_json: &str, policy: &str, plaintext: Vec<u8>) -> Result<String
 #[wasm_bindgen]
 pub fn keygen(pk_json: &str, msk_json: &str, pol_json: &str) -> Result<String, JsValue> {
     let pk: CpAbePublicKey = serde_json::from_str(pk_json)
-        .map_err(|e| JsValue::from_str(&format!("Error deserializing pk: {}", e)))?;
+        .map_err(|e| JsValue::from_str(&e.to_string()))?; 
 
     let msk: CpAbeMasterKey = serde_json::from_str(msk_json)
-        .map_err(|e| JsValue::from_str(&format!("Error deserializing msk: {}", e)))?;
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
 
     let pol_vec: Vec<String> = serde_json::from_str(pol_json)
-        .map_err(|e| JsValue::from_str(&format!("Error deserializing policy vector: {}", e)))?;
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
 
     let sk: CpAbeSecretKey = bsw::keygen(&pk, &msk, &pol_vec)
         .ok_or_else(|| JsValue::from_str("Key generation failed: SecretKey not generated"))?;
 
     serde_json::to_string(&sk)
-        .map_err(|e| JsValue::from_str(&format!("Error serializing secret key: {}", e)))
+        .map_err(|e| JsValue::from_str(&e.to_string()))
 }
 
 
 #[wasm_bindgen]
 pub fn decrypt(sk_json: &str, ct_cp_json: &str) -> Result<Uint8Array, JsValue> {
     let sk: CpAbeSecretKey = serde_json::from_str(sk_json)
-        .map_err(|e| JsValue::from_str(&format!("Error deserializing secret key: {}", e)))?;
+        .map_err(|e| JsValue::from_str(&format!("Error deserializing secret key: {:?}", e)))?;
 
     let ct_cp: CpAbeCiphertext = serde_json::from_str(ct_cp_json)
-        .map_err(|e| JsValue::from_str(&format!("Error deserializing ciphertext: {}", e)))?;
+        .map_err(|e| JsValue::from_str(&format!("Error deserializing ciphertext: {:?}", e)))?;
 
     let plaintext_bytes = bsw::decrypt(&sk, &ct_cp)
-        .map_err(|e| JsValue::from_str(&format!("Decryption error: {}", e)))?;
+        .map_err(|e| JsValue::from_str(&format!("Decryption error: {:?}", e)))?;
 
-    Ok(Uint8Array::new_with_length(plaintext_bytes.len() as u32).fill_with(&Clamped(&plaintext_bytes)))
+    Ok(Uint8Array::from(&plaintext_bytes[..]))
 }
