37fd9babd8ccf0515a614a955b82b9862434101a
ashleendaly <Ashleen.Daly@res-group.com>
2024-03-01 18:12:07 +0000

    remover thirdweb route

--- src/app/api/auth/[...thirdweb]/route.ts
+++
@@ -1,9 +0,0 @@
-import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
-import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
-
-export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  domain: process.env.APP_URL || "localhost:3000",
-  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
-});
-
-export default ThirdwebAuthHandler();
